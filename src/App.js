846d48114fee6fc9dad6680256daee84cd4d5fc5
Damien de Lartigue <damien@delartigue.co.uk>
2021-07-07 18:00:39 +0100

    fetch api data using axios

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends Component {
+    state = {
+        data: {}
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData })
+    }
+
     render(){
+        const {data} = this.state
+
         return(
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data}/>
                 <CountryPicker />
                 <Chart />
             </div>
